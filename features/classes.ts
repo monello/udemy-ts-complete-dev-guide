324601f30c57e29660e4ceb7aecfe64e652256b9
monello <louw.morne@gmail.com>
2022-10-29 14:33:35 +0200

    feat: s8v47(2) - Instance Method Modifiers - private

--- features/classes.ts
+++ features/classes.ts
@@ -1,19 +1,25 @@
 class Vehicle {
-  public drive(): void {
+  private drive(): void {
     console.log('chugga chugga');
   }
 
-  public honk(): void {
+  startDrivingProcess(): void {
+    this.drive();
+  }
+
+  honk(): void {
     console.log('beep');
   }
 }
 
 class MotorCar extends Vehicle {
-  public drive(): void {
-    console.log('vroom');
-  }
+  // had to remove this method - I don't think you can override private methods - will probably see in the next lesson
+  // private drive(): void {
+  //   console.log('vroom');
+  // }
 }
 
 const motorcar = new MotorCar();
-motorcar.drive();
+// motorcar.drive()                 // <<=== this is not possible anymore as drive was now set to private
+motorcar.startDrivingProcess();     // <<=== this is a function on the same class amd inherited by the child-class therefor it has access to call drive()
 motorcar.honk();

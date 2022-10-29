b9917dbc9e10976214f24379be896bf813ffeb53
monello <louw.morne@gmail.com>
2022-10-29 11:53:15 +0200

    feat: s7v41 - functions inside interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,19 +1,41 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  // anonymous function
+  summary(): string;
+  // arrow function
+  stats: () => string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date('2000-01-01'),
   broken: true,
+  // anonymous function
+  summary(): string {
+    return `
+    Name: ${this.name}
+    Year: ${this.year.getFullYear()}
+    `;
+  },
+  // arrow function
+  stats: (): string => {
+    return `
+    Power: 400 HP
+    Weight: 2000 kg
+    `;
+  }
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
   console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
+  console.log(`Year: ${vehicle.year.getFullYear()}`);
   console.log(`Broken: ${vehicle.broken}`);
 };
 
 printVehicle(oldCivic);
+
+console.log(oldCivic.summary());
+console.log(oldCivic.stats());
+

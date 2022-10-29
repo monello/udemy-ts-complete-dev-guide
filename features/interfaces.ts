bab503a4d53a30d16a783a96131c56b737d4baa9
monello <louw.morne@gmail.com>
2022-10-29 12:27:09 +0200

    feat: s7v43 - code reuse with interfaces

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -1,11 +1,3 @@
-// Note: an interface does NOT have to include ALL the properties an object can have.
-// It only describes the properties required for an object to be of this type.
-// So the oldCivic object below contains a lot more that just the summary function, but accoring to TypeScript
-//    for this object to be of type Reportable, the only question TS will ask is:
-//    "Does it have a property called summary, that is a function which returns a string", if yes...
-//    then it is a valid Reportable type, regardless of what other properties it contains
-// This is really cool, because it means that out interfaces can by much more concise, and only
-//    focus on the important parts that the Type needs
 interface Reportable {
   summary(): string;
 }
@@ -19,8 +11,32 @@ const oldCivic = {
   },
 };
 
+// The important thing to note here is that this bevarage object has a property called "summary", which is
+//    a function that returns a string.
+// This means it satisfies the criteria of the Reportable interface.
+// This is very cool, because this means that we can now apply the same interface to various, very-different objects
+//  (objects that describe very different things), but seeing as they both satisfy the Reportable interface, that are
+//  indee both reportable - we can call the summary function on them (see the last 2 lines of code below)
+const beverage = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  }
+};
+
+
+// The learning point for this lesson in the course was that by keeping our interfaces generic in it's shape and naming
+//    conventions, it can force (support) developers to write more generic and re-usable code:
+// - both the oldCivic and the bevarage objects have a summary function that returns a string, which in turn means
+// - we can now write a single re-usable funcion like "printSummary" (below), that can be used on ANY objecy that
+//    is of type Reportable.
+// Meaning it avoids situations where oldCivic has a summary() function and beverage has a nutrients() function
+//    which do the exact same thing: returns a string that summarizes the object
 const printSummary = (item: Reportable): void => {
   console.log(item.summary());
 };
 
 printSummary(oldCivic);
+printSummary(beverage);

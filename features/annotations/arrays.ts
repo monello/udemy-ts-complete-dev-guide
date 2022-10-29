266a9c0563f07207cc5fc3932f6ee5dd5faf1232
monello <louw.morne@gmail.com>
2022-10-29 08:07:33 +0200

    feat: s5v33 - multiple types in arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -16,3 +16,8 @@ carMakers.map(
         return car.toUpperCase();
     }
 );
+
+// Flexible types
+const importantDates: (Date | string)[] = [];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());

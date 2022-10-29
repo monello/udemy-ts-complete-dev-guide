e178482fef8158a4c9cc84017067377ff4559836
monello <louw.morne@gmail.com>
2022-10-29 15:09:10 +0200

    feat: s8v47(3) - Instance Method Modifiers - protected

--- features/classes.ts
+++ features/classes.ts
@@ -1,20 +1,19 @@
 class Vehicle {
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class MotorCar extends Vehicle {
   private drive(): void {
-    console.log('Vroom');
+    console.log('vroom');
   }
 
   startDrivingProcess(): void {
     this.drive();
+    this.honk();
   }
 }
 
 const motorcar = new MotorCar();
-// motorcar.drive()                 // <<=== this is not possible anymore as drive was now set to private
-motorcar.startDrivingProcess();     // <<=== this is a function on the same class amd inherited by the child-class therefor it has access to call drive()
-motorcar.honk();
+motorcar.startDrivingProcess();

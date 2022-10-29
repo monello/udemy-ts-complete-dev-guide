76da2e678b007eb4a7185edcf8e9358d1dfe3493
monello <louw.morne@gmail.com>
2022-10-29 08:57:21 +0200

    s6v36 - tuples in action

---
+++ features/annotations/tuples.ts
@@ -0,0 +1,16 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+};
+
+
+// annotating a tuple directly (without a type-alias)
+// const pepsi: [string, boolean, number] = ['brown', true, 40];
+
+// Type alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];

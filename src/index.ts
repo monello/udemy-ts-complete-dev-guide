fdc387a523dc69e62c56aed5311b191203efaa99
monello <louw.morne@gmail.com>
2022-10-31 15:58:16 +0200

    feat: s9v64 - implemented the google maps ui

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
-import { User } from "./User";
-import { Company } from "./Company";
+// import { User } from "./User";
+// import { Company } from "./Company";
 
-const user = new User();
-console.log("user:", user);
+// Initialize and add the map
+function initMap(): void {
+    // The location of Uluru
+    // const start_position = { lat: -25.344, lng: 131.031 };
+    const start_position = { lat: 0, lng: 0 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(
+        // MRL: The target HTML-Element to load the map UI in
+        document.getElementById("map") as HTMLElement,
+        {
+            zoom: 1,
+            center: start_position,
+        }
+    );
 
-const company = new Company();
-console.log("company:", company);
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+        position: start_position,
+        map: map,
+    });
+}
 
+declare global {
+    interface Window {
+        initMap: () => void;
+    }
+}
+window.initMap = initMap;
 
+export { };

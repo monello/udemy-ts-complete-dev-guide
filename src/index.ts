70bcad912cc7ab85392950368cab13db00810506
monello <louw.morne@gmail.com>
2022-11-04 16:33:50 +0200

    feat: s10v81 - basic bubble-sort implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hi there!');
+class Sorter {
+    constructor(public collection: number[]) { }
+
+    sort(): void {
+        // destructoring the "length" property off the collection array
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0, 11]);
+sorter.sort();
+console.log(sorter.collection);

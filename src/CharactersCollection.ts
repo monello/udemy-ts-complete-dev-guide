cce8486536048855810fb934a1197795d0bd3865
monello <louw.morne@gmail.com>
2022-11-05 13:13:42 +0200

    refactor: s10v90 (3) - add the compare method

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -4,4 +4,14 @@ export class CharactersCollection {
     get length(): number {
         return this.data.length;
     }
+
+    // this function simply needs to return a boolean
+    // true if the left character is alphabetically higher that the right
+    // false if not
+    compare(leftIndex: number, rightIndex: number): boolean {
+        // the character code range of uppercase letters (characters) is lower than that of the lowercase letters
+        // Seeing as all we want to know is if the left letter is alpahabetically higher than the righr (irrespective of casing)
+        //   we force both letters to lowercase
+        return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
+    }
 }

8fd30d7ebd860ff7779ee3e7312c91e33276fd0b
monello <louw.morne@gmail.com>
2022-11-05 16:15:46 +0200

    refactor: s10v95 (1) - update numberscollection to extend (inherit from) sorter

--- src/NumbersCollection.ts
+++ src/NumbersCollection.ts
@@ -1,16 +1,6 @@
 export class NumbersCollection {
     constructor(public data: number[]) { }
 
-    // I'm not sure why the lecturer didn't just do the following instead of the getter/accessor below.
-    // Is an access better practice, or was it just for educational purposes (he didn't say)
-    // length: number = this.data.length;
-
-    // when adding the "get" keyword in front of a class method you are creating an accessor,
-    // more specifically a getter.
-    // As far as I know the puspose of this you normally be to give other classes controlled
-    // access to private and protected properties and to give the external classes a way to
-    // access a complex property with a simple propery notation: objectName.property instead
-    // of objectName/property()
     get length(): number {
         return this.data.length;
     }

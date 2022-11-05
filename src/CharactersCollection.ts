c9bb9c49301cbc1e69c2bc754b4835390838ef3d
monello <louw.morne@gmail.com>
2022-11-05 14:06:24 +0200

    refactor: s10v90 (4) - add the swap method

--- src/CharactersCollection.ts
+++ src/CharactersCollection.ts
@@ -14,4 +14,15 @@ export class CharactersCollection {
         //   we force both letters to lowercase
         return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
     }
+
+    swap(leftIndex: number, rightIndex: number): void {
+        // strings are an immutable data type, so we cannot swop characters i=on the string itself
+        // a workaround is to split the string int an array of characters, do the swapping there
+        // then join the array of characters back into a string and ovberride the original string
+        const characters = this.data.split('');
+        const leftHand = characters[leftIndex];
+        characters[leftIndex] = characters[rightIndex];
+        characters[rightIndex] = leftHand;
+        this.data = characters.join('');
+    }
 }

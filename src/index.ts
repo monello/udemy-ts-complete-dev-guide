20802ff6d9c5127161f184ad18570200677aeb8e
monello <louw.morne@gmail.com>
2022-11-05 18:43:26 +0200

    refactor: s10v98 (5) - refactor and test string-collection and linked-lists

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
@@ -7,16 +6,14 @@ const numbersCollection = new NumbersCollection([10, 3, -5, 0, 11]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('XaAopb');
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('XaAopb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(500);
-// linkedList.add(-10);
-// linkedList.add(-3);
-// linkedList.add(4);
-// const sorter = new Sorter(linkedList);
-// sorter.sort();
-// linkedList.print();
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.sort();
+linkedList.print();

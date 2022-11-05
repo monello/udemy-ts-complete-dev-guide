b0e49cc1333e01d235baa25680af3a0de4201f27
monello <louw.morne@gmail.com>
2022-11-05 17:01:08 +0200

    refactor: s10v95 (2) - remove the collection property from the sort class

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -5,16 +5,14 @@ export interface Sortable {
 }
 
 export class Sorter {
-    constructor(public collection: Sortable) { }
-
     sort(): void {
-        // destructoring the "length" property off the collection array
-        const { length } = this.collection;
+        // destructoring the "length" property off the collection that inherited from thsi class
+        const { length } = this;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }

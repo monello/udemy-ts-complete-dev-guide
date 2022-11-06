148876b2c0235426d06e5301562066cd59d4b6bb
monello <louw.morne@gmail.com>
2022-11-06 08:06:35 +0200

    feat: s11v104 - reading contents of a csv file into a string variable

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+    // The 'encoding' setting tells fs what kind of file we are trying to read, by telling it the encoding of the file content.
+    // In this case text encoded as "utf-8"
+    encoding: 'utf-8',
+});
+
+console.log(matches);

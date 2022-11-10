0979bb9acb0851b418a1b971c30868bdbb39eaea
monello <louw.morne@gmail.com>
2022-11-10 18:46:03 +0200

    refactor: s11v125 (4) - brining it all together

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// 1. Create an object that satisfies the 'DataReader' interfacs
+const csvFileReader = new CsvFileReader('football.csv');
+
+// 2. Create an instance of the 'MatchReader' class and pass in something
+//    satisfying the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+// 3. Load (and transform) the data
+matchReader.load();
 
 // Some basic data analysis
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+// 4. Update the reference from where to get the data to report on
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {

9958fb22b4b0bfcda0115e394a5933bf8a93b64d
monello <louw.morne@gmail.com>
2022-11-10 18:28:41 +0200

    refactor: s11v124 (3) - transforming data

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -9,18 +9,23 @@ interface DataReader {
 }
 
 export class MatchReader {
+    matches: MatchData[] = [];
+
     constructor(public reader: DataReader) { }
 
+    load(): void {
+        this.reader.read();
+        this.matches = this.reader.data.map((row: string[]): MatchData => {
+            return [
+                dateStringToDate(row[0]),
+                row[1],
+                row[2],
+                +row[3],
+                +row[4],
+                row[5] as MatchResult,
+                row[6]
+            ];
+        });
+    }
 
-    //     .map((row: string[]): MatchData => {
-    //     return [
-    //         dateStringToDate(row[0]),
-    //         row[1],
-    //         row[2],
-    //         +row[3],
-    //         +row[4],
-    //         row[5] as MatchResult,
-    //         row[6]
-    //     ];
-    // });
 }

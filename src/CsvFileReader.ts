b3df7263fbf2251741f584ab9d8c52957b6ef318
monello <louw.morne@gmail.com>
2022-11-10 17:49:23 +0200

    refactor: s11v123 (2) - move all references to match specific data out of the csvfilereader class

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,7 @@
 import fs from 'fs';
 
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
 export class CsvFileReader {
-    data: MatchData[] = [];
+    data: string[][] = [];
 
     constructor(public filename: string) { }
 
@@ -16,17 +11,6 @@ export class CsvFileReader {
                 encoding: 'utf-8',
             })
             .split('\n')
-            .map((row: string): string[] => row.split(','))
-            .map((row: string[]): MatchData => {
-                return [
-                    dateStringToDate(row[0]),
-                    row[1],
-                    row[2],
-                    +row[3],
-                    +row[4],
-                    row[5] as MatchResult,
-                    row[6]
-                ];
-            });
+            .map((row: string): string[] => row.split(','));
     }
 }
--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,3 +1,8 @@
+import { dateStringToDate } from './utils';
+import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 interface DataReader {
     read(): void;
     data: string[][];
@@ -5,4 +10,17 @@ interface DataReader {
 
 export class MatchReader {
     constructor(public reader: DataReader) { }
+
+
+    //     .map((row: string[]): MatchData => {
+    //     return [
+    //         dateStringToDate(row[0]),
+    //         row[1],
+    //         row[2],
+    //         +row[3],
+    //         +row[4],
+    //         row[5] as MatchResult,
+    //         row[6]
+    //     ];
+    // });
 }

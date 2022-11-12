4bc0bbbf9c436f80bc338643ebcfaa19465934d5
monello <louw.morne@gmail.com>
2022-11-12 22:50:30 +0200

    refactor: s11v132 (2) - the starter code for the summary (main/coordinator) class

---
+++ src/Summary.ts
@@ -0,0 +1,25 @@
+import { MatchData } from "./MatchData";
+
+/*
+    NOTE:
+    Here we export the interfaces, because we want to specifically implement it in other classes to get some additional TypeScript support.
+    Explicitly "implementing" the interfaces on class definitions is totally optiona;, because TS will automatically (implictly) make sure these
+        classes comply with these interfaces when we try and use them where the interfaces are applicable.
+    The benefit of implementing the interface explicitly on a class definition ensures we make this class comply with the interface at
+        the of coding it up.
+*/
+
+export interface Analyzer {
+    run(matches: MatchData[]): string;
+}
+
+export interface OutputTarget {
+    print(report: string): void;
+}
+
+// So here we see the whole KEY TO UNDERSTANING object composition:
+// We pass in REFERENCES to the objects that contain the specific logic as ARGUMENTS to this class. See the arguments to the constructor()
+// This will allow us to call theit methods on these object instances (DELEGATION)
+export class Summary {
+    constructor(public analyzer: Analyzer, public outputTarget: OutputTarget) { }
+}

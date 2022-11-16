6b2c5d5030eae5da6a7ead2fcc15e356571c6cb9
monello <louw.morne@gmail.com>
2022-11-16 06:30:34 +0200

    s13v148 - add a set function to upate data

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { User } from "./models/User";
 const user = new User({ name: 'Morne', age: 48 });
 console.log(`Name: ${user.get('name')}
 Age: ${user.get('age')}`);
+
+user.set({ name: 'Morne Riaan', age: 21 });
+console.log(`Name: ${user.get('name')}
+Age: ${user.get('age')}`);
--- src/models/User.ts
+++ src/models/User.ts
@@ -8,4 +8,8 @@ export class User {
     get(propName: string): (number | string) {
         return this.data[propName];
     }
+
+    set(update: UserProps): void {
+        Object.assign(this.data, update);
+    }
 }

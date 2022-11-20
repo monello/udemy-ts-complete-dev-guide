04f3a585957b0be7d848b30b09aaab55a4d91e7e
monello <louw.morne@gmail.com>
2022-11-20 05:11:31 +0200

    s13v151 - eventing system (2) - somewhere to store events

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,7 +6,12 @@ export interface UserProps {
 type Callback = () => {};
 
 export class User {
+    // [key: string] is a TypeScript "index signature", we use this when we don't know what
+    //   all the property names will be in the future, but we do know they will be strings
+    events: { [key: string]: Callback[]; } = {};
+
     constructor(private data: UserProps) { }
+
     get(propName: string): (number | string) {
         return this.data[propName];
     }

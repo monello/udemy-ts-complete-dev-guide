452f68bbbb70c72a04edf06e4957da19efef0a67
monello <louw.morne@gmail.com>
2022-11-27 07:01:46 +0200

    feat: s13v155 - example get request - all users and single user

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.post('http://localhost:3000/users', {
-    name: "morne",
-    age: 48
-});
+// get all the users
+axios.get('http://localhost:3000/users');
+
+// get a specific user (with id = 1)
+axios.get('http://localhost:3000/users/1');
